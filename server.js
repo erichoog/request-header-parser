ed2a932466f5a11a17a5606c9222317cc3c931d4
Eric <eric_hoog@hotmail.com>
2016-08-28 20:14:02 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.locals.pretty = true;
+app.set('port', (process.env.PORT || 8080));
+
+app.get('/api/whoami', function (req, res) {
+  
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var lang = req.headers["accept-language"];
+  var ua = req.headers['user-agent'];
+  
+  var obj = {
+    ipaddress: ip,
+    language : lang,
+    software: ua
+  };
+  
+  res.json(obj);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
